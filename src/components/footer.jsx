dcd73b344822d4bb1be5c70a016ffcbf9b7c3ec1
Andy <qingjielu990916@gmail.com>
2021-10-22 09:44:17 +0800

    first commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+export const Footer = (props) => {
+  return (
+    <div>
+      <div id="footer">
+        <div className="container text-center">
+          <p>&copy; 2021 MIT Research Group 3</p>
+        </div>
+      </div>
+    </div>
+  );
+};
